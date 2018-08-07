c6c2417b9bf39b45fe4f29c97fe18d0a76cd19dc
Gregory Smith <gregsmith446@gmail.com>
2018-08-07 17:33:36 -0400

    completed tutorial

---
+++ Fizz_Buzz2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FizzBuzz.Library;
+
+namespace Fizz_Buzz
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(FizzBuzzer.GetValue(i));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
